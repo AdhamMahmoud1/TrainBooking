24be3bf69a0689c0ed7aa0f084f12e002fdfd2eb
Fady-K <popfadykamal151617@gmail.com>
2023-05-21 02:30:55 +0300

    updated

--- TrainBookingSystem/TrainBookingSystem/Program.cs
+++ TrainBookingSystem/TrainBookingSystem/Program.cs
@@ -1,3 +1,6 @@
+using TrainBookingSystem.Forms;
+using TrainBookingSystem.Services;
+
 namespace TrainBookingSystem
 {
     internal static class Program
@@ -11,7 +14,9 @@ namespace TrainBookingSystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Login());
+
+            
         }
     }
 }
