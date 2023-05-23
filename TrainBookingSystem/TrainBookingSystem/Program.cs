e4b8faf5f0be0ca919ee733151f1c9deb5b4f85a
Adel Magdy <101670025+AdelMagdy22@users.noreply.github.com>
2023-05-23 16:20:44 +0300

    Update Program.cs

--- TrainBookingSystem/TrainBookingSystem/Program.cs
+++ TrainBookingSystem/TrainBookingSystem/Program.cs
@@ -14,8 +14,7 @@ namespace TrainBookingSystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //Application.Run(new Login());
-            Application.Run(new AdminForm(new Admin()));
+            Application.Run(new Login());
         }
     }
 }
