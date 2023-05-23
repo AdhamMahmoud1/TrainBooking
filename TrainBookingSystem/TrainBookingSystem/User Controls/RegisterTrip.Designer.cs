a3d3a8fe8c9f030a80b5b6285e2a66ece668e573
Fady-K <popfadykamal151617@gmail.com>
2023-05-23 13:57:42 +0300

    modified

--- TrainBookingSystem/TrainBookingSystem/User Controls/RegisterTrip.Designer.cs
+++ TrainBookingSystem/TrainBookingSystem/User Controls/RegisterTrip.Designer.cs
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.comboBoxTo = new System.Windows.Forms.ComboBox();
             this.comboBoxFrom = new System.Windows.Forms.ComboBox();
-            this.dataGridViewTripsWithSourceAndDistination = new System.Windows.Forms.DataGridView();
+            this.comboBoxTo = new System.Windows.Forms.ComboBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.buttonRegisterTrip = new System.Windows.Forms.Button();
+            this.dataGridViewTripsWithSourceAndDistination = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTripsWithSourceAndDistination)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,57 +53,6 @@
             this.panel1.Size = new System.Drawing.Size(977, 571);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 528);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(977, 43);
-            this.panel2.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(353, 10);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(238, 23);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Copyrights @ FCAI 2023";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(353, 10);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(238, 23);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Copyrights @ FCAI 2023";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(92)))), ((int)(((byte)(194)))));
-            this.panel3.Controls.Add(this.comboBoxTo);
-            this.panel3.Controls.Add(this.comboBoxFrom);
-            this.panel3.Location = new System.Drawing.Point(0, 3);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(977, 70);
-            this.panel3.TabIndex = 2;
-            // 
-            // comboBoxTo
-            // 
-            this.comboBoxTo.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.comboBoxTo.FormattingEnabled = true;
-            this.comboBoxTo.Location = new System.Drawing.Point(748, 20);
-            this.comboBoxTo.Name = "comboBoxTo";
-            this.comboBoxTo.Size = new System.Drawing.Size(183, 31);
-            this.comboBoxTo.TabIndex = 0;
-            this.comboBoxTo.Text = "To";
-            this.comboBoxTo.SelectedIndexChanged += new System.EventHandler(this.comboBoxTo_SelectedIndexChanged);
-            // 
             // comboBoxFrom
             // 
             this.comboBoxFrom.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
@@ -116,18 +65,56 @@
             this.comboBoxFrom.Text = "From";
             this.comboBoxFrom.SelectedIndexChanged += new System.EventHandler(this.comboBoxFrom_SelectedIndexChanged);
             // 
-            // dataGridViewTripsWithSourceAndDistination
+            // comboBoxTo
             // 
-            this.dataGridViewTripsWithSourceAndDistination.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.dataGridViewTripsWithSourceAndDistination.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridViewTripsWithSourceAndDistination.BackgroundColor = System.Drawing.Color.White;
-            this.dataGridViewTripsWithSourceAndDistination.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewTripsWithSourceAndDistination.Location = new System.Drawing.Point(0, 79);
-            this.dataGridViewTripsWithSourceAndDistination.Name = "dataGridViewTripsWithSourceAndDistination";
-            this.dataGridViewTripsWithSourceAndDistination.RowHeadersWidth = 51;
-            this.dataGridViewTripsWithSourceAndDistination.RowTemplate.Height = 29;
-            this.dataGridViewTripsWithSourceAndDistination.Size = new System.Drawing.Size(977, 370);
-            this.dataGridViewTripsWithSourceAndDistination.TabIndex = 3;
+            this.comboBoxTo.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.comboBoxTo.FormattingEnabled = true;
+            this.comboBoxTo.Location = new System.Drawing.Point(748, 20);
+            this.comboBoxTo.Name = "comboBoxTo";
+            this.comboBoxTo.Size = new System.Drawing.Size(183, 31);
+            this.comboBoxTo.TabIndex = 0;
+            this.comboBoxTo.Text = "To";
+            this.comboBoxTo.SelectedIndexChanged += new System.EventHandler(this.comboBoxTo_SelectedIndexChanged);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(92)))), ((int)(((byte)(194)))));
+            this.panel3.Controls.Add(this.comboBoxTo);
+            this.panel3.Controls.Add(this.comboBoxFrom);
+            this.panel3.Location = new System.Drawing.Point(0, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(977, 70);
+            this.panel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(353, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Copyrights @ FCAI 2023";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(353, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(238, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Copyrights @ FCAI 2023";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 528);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(977, 43);
+            this.panel2.TabIndex = 1;
             // 
             // buttonRegisterTrip
             // 
@@ -145,6 +132,19 @@
             this.buttonRegisterTrip.UseVisualStyleBackColor = false;
             this.buttonRegisterTrip.Click += new System.EventHandler(this.buttonRegisterTrip_Click);
             // 
+            // dataGridViewTripsWithSourceAndDistination
+            // 
+            this.dataGridViewTripsWithSourceAndDistination.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridViewTripsWithSourceAndDistination.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewTripsWithSourceAndDistination.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewTripsWithSourceAndDistination.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTripsWithSourceAndDistination.Location = new System.Drawing.Point(0, 79);
+            this.dataGridViewTripsWithSourceAndDistination.Name = "dataGridViewTripsWithSourceAndDistination";
+            this.dataGridViewTripsWithSourceAndDistination.RowHeadersWidth = 51;
+            this.dataGridViewTripsWithSourceAndDistination.RowTemplate.Height = 29;
+            this.dataGridViewTripsWithSourceAndDistination.Size = new System.Drawing.Size(977, 370);
+            this.dataGridViewTripsWithSourceAndDistination.TabIndex = 3;
+            // 
             // RegisterTrip
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
@@ -160,9 +160,9 @@
             this.Size = new System.Drawing.Size(977, 571);
             this.Load += new System.EventHandler(this.RegisterTrip_Load);
             this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            this.panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTripsWithSourceAndDistination)).EndInit();
             this.ResumeLayout(false);
 
@@ -171,13 +171,13 @@
         #endregion
 
         private Panel panel1;
-        private Panel panel2;
-        private Label label2;
-        private Label label1;
-        private Panel panel3;
-        private ComboBox comboBoxTo;
-        private ComboBox comboBoxFrom;
-        private DataGridView dataGridViewTripsWithSourceAndDistination;
         private Button buttonRegisterTrip;
+        private ComboBox comboBoxFrom;
+        private ComboBox comboBoxTo;
+        private Panel panel3;
+        private Label label1;
+        private Label label2;
+        private Panel panel2;
+        private DataGridView dataGridViewTripsWithSourceAndDistination;
     }
 }
